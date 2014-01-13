e19e637ff3ff3aa57827f5e4be30010b1aabbcfd
Joel Pearson <virtuosojoel@gmail.com>
2014-01-13 09:48:56 +0000

    Updated query string

--- Unallocate/Simple Unallocate.rb
+++ Unallocate/Simple Unallocate.rb
@@ -39,6 +39,8 @@ class UnallocateTool
       # send the request to the server
       m.agent.get query
       
+      puts 'Unassigned ' + query[ 'bc' ]
+      
     end # loop
     
   rescue => error
